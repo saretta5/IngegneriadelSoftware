43617d2f8067663c76afd6c00f531e6fa1d7c380
saretta5 <95508733+saretta5@users.noreply.github.com>
2021-12-16 14:15:11 +0100

    viis

--- src/calcolamod.js
+++ src/calcolamod.js
@@ -6,7 +6,22 @@ function calcola() {
   var sagg = document.getElementById("sag").value;
   var cari = document.getElementById("car").value;
 
-  var tmp = Math.trunc(parseInt(forz, 10) - 10);
-
-  console.log(tmp);
+  document.getElementById("mfor").value = Math.trunc(
+    (parseInt(forz, 10) - 10) / 2
+  );
+  document.getElementById("mdes").value = Math.trunc(
+    (parseInt(dest, 10) - 10) / 2
+  );
+  document.getElementById("mcos").value = Math.trunc(
+    (parseInt(cost, 10) - 10) / 2
+  );
+  document.getElementById("mint").value = Math.trunc(
+    (parseInt(inte, 10) - 10) / 2
+  );
+  document.getElementById("msag").value = Math.trunc(
+    (parseInt(sagg, 10) - 10) / 2
+  );
+  document.getElementById("mcar").value = Math.trunc(
+    (parseInt(cari, 10) - 10) / 2
+  );
 }
